28bc55da821b9c873b4f51ad1ea31ec7e4249a28
asabelo <asabelo@proton.me>
2024-09-29 17:43:11 +0200

    4.1.0 The Interpreter Framework

---
+++ C#Lox/Lox.cs
@@ -0,0 +1,52 @@
+﻿switch (args.Length)
+{
+    case > 1:
+        Console.WriteLine("Usage: c#lox [script]");
+        Environment.Exit(64);
+        break;
+
+    case 1:
+        await RunFile(args[0]);
+        break;
+
+    default:
+        RunPrompt();
+        break;
+}
+
+// Read and execute source file 
+async Task RunFile(string filePath)
+{
+    var code = await File.ReadAllTextAsync(filePath);
+    Run(code);
+}
+
+// Start a REPL
+void RunPrompt()
+{
+    var keepGoing = true;
+
+    while (keepGoing)
+    {
+        Console.Write("> ");
+
+        var code = Console.ReadLine();
+
+        if (code is null)
+        {
+            keepGoing = false;
+        }
+        else
+        {
+            Run(code);
+        }
+    }
+}
+
+void Run(string code)
+{
+    foreach (var token in code.Split(' '))
+    {
+        Console.WriteLine(token);
+    }
+}
