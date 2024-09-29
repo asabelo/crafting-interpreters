687c953cf18eeb96189e446aa3d376e221dd1453
asabelo <asabelo@proton.me>
2024-09-29 19:37:51 +0200

    4.2.3 Location information

---
+++ C#Lox/Token.cs
@@ -0,0 +1,15 @@
+namespace Lox;
+
+record Token
+(
+    TokenType Type,
+    string Lexeme,
+    object Literal,
+    int Line
+)
+{
+    public override string ToString()
+    {
+        return $"{Type} {Lexeme} {Literal}";
+    }
+}
