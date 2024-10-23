09306128b85348bda17cd317bdb179697e43408b
asabelo <asabelo@proton.me>
2024-10-23 21:47:49 +0200

    VM class, move constructor and assignment for array

--- C++Lox/main.cpp
+++ C++Lox/main.cpp
@@ -1,18 +1,21 @@
 
-#include "common.hpp"
 #include "chunk.hpp"
+#include "common.hpp"
 #include "debug.hpp"
+#include "vm.hpp"
 
 int main(int argc, char* argv[])
 {
-    auto c = lox::chunk{};
+    auto vm = lox::vm{};
 
-    auto constant = c.constants().add(1.2);
+    auto c = lox::chunk{};
     c.add(lox::op_code::OP_CONSTANT, 123);
-    c.add(constant, 123);
+    c.add(c.constants().add(1.2), 123);
     c.add(lox::op_code::OP_RETURN, 123);
 
     lox::disassemble_chunk(c, "test chunk");
+
+    vm.interpret(std::move(c));
 
     return 0;
 }
