2361de371d9febe9d930dc12190f8af853ead2fb
asabelo <asabelo@proton.me>
2024-10-23 15:24:21 +0200

    Pass index to array::get and return reference instead of a pointer

--- C++Lox/debug.cpp
+++ C++Lox/debug.cpp
@@ -12,41 +12,41 @@ void lox::disassemble_chunk(const chunk& chunk, const char* name)
     }
 }
 
-static int constant_instruction(const char* name, const lox::chunk& chunk, int offset)
+static lox::chunk::idx_t constant_instruction(const char* name, const lox::chunk& chunk, lox::chunk::idx_t offset)
 {
-    auto constant = chunk.get()[offset + 1];
+    auto constant = chunk.get(offset + 1);
     
     std::printf("%-16s %4d '", name, constant);
     
-    lox::print_value(chunk.constants().get()[constant]);
+    lox::print_value(chunk.constants().get(constant));
     
     std::printf("'\n");
 
     return offset + 2;
 }
 
-static int simple_instruction(const char* name, int offset)
+static lox::chunk::idx_t simple_instruction(const char* name, lox::chunk::idx_t offset)
 {
     std::printf("%s\n", name);
 
     return offset + 1;
 }
 
-int lox::disassemble_instruction(const chunk& chunk, int offset)
+int lox::disassemble_instruction(const chunk& chunk, chunk::idx_t offset)
 {
-    std::printf("%04d ", offset);
+    std::printf("%04d ", static_cast<int>(offset));
 
-    auto lines = chunk.lines().get();
-    if (offset > 0 && lines[offset] == lines[offset - 1])
+    auto& lines = chunk.lines();
+    if (offset > 0 && lines.get(offset) == lines.get(offset - 1))
     {
         printf("   | ");
     }
     else
     {
-        printf("%4d ", lines[offset]);
+        printf("%4d ", lines.get(offset));
     }
 
-    auto instruction = chunk.get()[offset];
+    auto instruction = chunk.get(offset);
 
     switch (instruction)
     {
