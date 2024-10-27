b478bbe9e009940afa0b99553a9f6c09eb3f7a96
asabelo <asabelo@proton.me>
2024-10-27 11:21:29 +0100

    Extract vm implementation to source file

---
+++ C++Lox/vm.cpp
@@ -0,0 +1,88 @@
+
+#include "vm.hpp"
+
+#include <functional>
+
+#include "compiler.hpp"
+#include "debug.hpp"
+
+lox::interpret_result lox::vm::run()
+{
+    const auto read_byte = [this]()
+    {
+        return chunk.get(ip++);
+    };
+
+    const auto read_constant = [this, &read_byte]()
+    {
+        return chunk.constants().get(read_byte());
+    };
+
+    const auto binary_op = [this](std::function<value(value, value)> op)
+    {
+        const auto b = stack.pop();
+        const auto a = stack.pop();
+        return stack.push(op(a, b));
+    };
+
+    while (true)
+    {
+#ifdef _DEBUG
+        std::cout << "          ";
+
+        for (lox::stack<value>::idx_t i = 0; i < stack.count(); ++i)
+        {
+            std::cout << "[ ";
+
+            print_value(stack.get(i));
+
+            std::cout << " ]";
+        }
+
+        std::cout << '\n';
+
+        disassemble_instruction(chunk, ip);
+#endif // DEBUG
+
+        switch (read_byte())
+        {
+        case op_code::OP_CONSTANT:
+            stack.push(read_constant());
+            break;
+
+        case op_code::OP_ADD:
+            binary_op(std::plus{});
+            break;
+
+        case op_code::OP_SUBTRACT:
+            binary_op(std::minus{});
+            break;
+
+        case op_code::OP_MULTIPLY:
+            binary_op(std::multiplies{});
+            break;
+
+        case op_code::OP_DIVIDE:
+            binary_op(std::divides{});
+            break;
+
+        case op_code::OP_NEGATE:
+            stack.push(-stack.pop());
+            break;
+
+        case op_code::OP_RETURN:
+            print_value(stack.pop());
+            std::cout << '\n';
+            return interpret_result::INTERPRET_OK;
+        }
+    }
+}
+
+lox::interpret_result lox::vm::interpret(const std::string_view source)
+{
+    auto scanner = lox::scanner{ source };
+
+    compile(scanner);
+
+    return interpret_result::INTERPRET_OK;
+}
--- C++Lox/vm.hpp
+++ C++Lox/vm.hpp
@@ -1,12 +1,8 @@
 
 #pragma once
 
-#include <functional>
-
 #include "chunk.hpp"
 #include "common.hpp"
-#include "compiler.hpp"
-#include "debug.hpp"
 #include "stack.hpp"
 
 namespace lox
@@ -26,87 +22,10 @@ namespace lox
 
         stack<value> stack = {};
 
-        interpret_result run()
-        {
-            const auto read_byte = [this]()
-            {
-                return chunk.get(ip++);
-            };
-
-            const auto read_constant = [this, &read_byte]()
-            {
-                return chunk.constants().get(read_byte());
-            };
-
-            const auto binary_op = [this](std::function<value(value, value)> op)
-            {
-                const auto b = stack.pop();
-                const auto a = stack.pop();
-                return stack.push(op(a, b));
-            };
-
-            while (true)
-            {
-#ifdef _DEBUG
-                std::cout << "          ";
-
-                for (lox::stack<value>::idx_t i = 0; i < stack.count(); ++i)
-                {
-                    std::cout << "[ ";
-
-                    print_value(stack.get(i));
-
-                    std::cout << " ]";
-                }
-
-                std::cout << '\n';
-
-                disassemble_instruction(chunk, ip);
-#endif // DEBUG
-
-                switch (read_byte())
-                {
-                case op_code::OP_CONSTANT:
-                    stack.push(read_constant());
-                    break;
-
-                case op_code::OP_ADD:
-                    binary_op(std::plus{});
-                    break;
-
-                case op_code::OP_SUBTRACT:
-                    binary_op(std::minus{});
-                    break;
-
-                case op_code::OP_MULTIPLY:
-                    binary_op(std::multiplies{});
-                    break;
-
-                case op_code::OP_DIVIDE:
-                    binary_op(std::divides{});
-                    break;
-
-                case op_code::OP_NEGATE:
-                    stack.push(-stack.pop());
-                    break;
-
-                case op_code::OP_RETURN:
-                    print_value(stack.pop());
-                    std::cout << '\n';
-                    return interpret_result::INTERPRET_OK;
-                }
-            }
-        }
+        interpret_result run();
 
     public:
 
-        interpret_result interpret(const std::string_view source)
-        {
-            auto scanner = lox::scanner{ source };
-
-            compile(scanner);
-
-            return interpret_result::INTERPRET_OK;
-        }
+        interpret_result interpret(const std::string_view source);
     };
 }
